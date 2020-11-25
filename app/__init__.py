7a2bdf7eadf92910d5b95417f5e0ea299594880e
2206946486 <404449416@qq.com>
2020-11-25 17:48:57 +0800

    最基本的请求路由跑起来

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,9 @@
 @Author  : LJ
 @Time    : 2020/11/25 16:31
 """
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    return app
--- wsgi.py
+++ wsgi.py
@@ -3,3 +3,16 @@
 @Author  : LJ
 @Time    : 2020/11/25 16:31
 """
+from app import create_app
+
+
+app = create_app()
+
+
+@app.route('/')
+def test():
+    return '测试一次啊'
+
+
+if __name__ == "__main__":
+    app.run()
